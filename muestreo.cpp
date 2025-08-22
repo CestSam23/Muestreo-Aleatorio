ddf496b5eeaed70e67c8e5b574062c25c3f933a4
Sam <samfertap@gmail.com>
2025-08-21 21:49:23 -0600

    Added Binomial
    Let x be the number of successes. Where x=1 succcess, x=0 failure-
    Theta = probability of success
    n = size of the samples
    k = number of trial

--- muestreo.cpp
+++ muestreo.cpp
@@ -2,18 +2,35 @@
 
 extern "C" {
     void muestreoBernulli(double *success, double *failure, 
-        double probabilitySuccess, int sizeOfSample){
-        for(int i=0;i<sizeOfSample;i++){
-            if((double)rand()/(RAND_MAX)<probabilitySuccess){
+        double theta, int n){
+        /*Let x be the number of successes. Where x=1 success, x=0 failure 
+        theta = probability of success
+        n = size of the sample
+        */
+        for(int i=0;i<n;i++){
+            if((double)rand()/(RAND_MAX)<theta){
                 (*success)++;
             }else{
                 (*failure)++;
             }
         }
     }
-    void muestreoBinomial(){
-
+    void muestreoBinomial(double *success, double theta, 
+        int n, int k){
+        /*Let x be the number of successes. Where x=1 success, x=0 failure
+        theta = probability of success
+        n = size of the samples
+        k = number of trials
+        */
+       for (int i = 0; i < k; i++){
+           for(int j=0;j<n;j++){
+               if((double)rand()/(RAND_MAX)<theta){
+                   success[i]++;
+               }
+           }
+       }
     }
+    
     void muestreoMultinomial(){
 
     }
