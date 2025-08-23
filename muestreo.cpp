1906b5459b4647fd0cfcdd0cc32072c9a8391176
Sam <samfertap@gmail.com>
2025-08-23 17:35:14 -0600

    Added the multinomial functions
    Added
    -belongsTo(double *thetas);
    	Function that is used in multinomial functions.
    	It generates the random number and return the position in thetas in which
    	the value belongs to.
    
    -muestreoMultinomial (Overloaded)
    	The first one receives an integer number in which the probabilities are going to be distributed
    	The second one receives a vector or unidimensional array with probabilities.
    	The sum of this probabilities must be 1.
    	Ex.
    	thetas = {.10,.20,.50,.10,.10}
    	sum(thetas) = 1.

--- muestreo.cpp
+++ muestreo.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+int belongsTo(double *thetas){
+    /*Private function. Called on multinomial.
+    Return the position in which a random number is generated in a set of a sample*/
 
+    int position = 0, accumulated = 0;
+    while(true){
+        if((double)rand()/RAND_MAX>accumulated &&
+            (double)rand()/RAND_MAX<thetas[position]+accumulated){
+            return position;
+        }
+        accumulated += thetas[position];
+        position++;
+    }
+    
+}
 extern "C" {
     void muestreoBernulli(double *success, double *failure, 
         double theta, int n){
@@ -30,8 +46,47 @@ extern "C" {
            }
        }
     }
-    
-    void muestreoMultinomial(){
+
+    //Muestreo con partes iguales
+    void muestreoMultinomial(int slices, int n, int k,double **results){
+        /*Let Slices be the probability of each case in the experiment
+        where A1+A2+...+Aslices=1. 1/slices. (Same probability for each one)
+        Let n be the size of the samples
+        Let k be the number of trials
+
+        Results is a vector of R2, where R2[i] is the number of successes for the i-th slice
+        */
+       
+        vector<double> r(slices,0.0);
+        for(int i=0;i<slices;i++){
+            r[i] = 1.0/slices;
+        }
+
+        for(int i=0;i<k;i++){
+            for(int j=0;j<n;j++){
+                results[i][belongsTo(r.data())]++;
+            }
+        }
+
+    }
+
+    //Muestreo con n probabilidad
+    void muestreoMultinomial(double *thetas, int n, int k, double **results){
+        /*Let thetas be the probability of each case in the experiment
+        where A1+A2+...+Aslices=1. (Different probability for each one).
+        (Array of double).
+        Let n be the size of the samples
+        Let k be the number of trials
+
+        Results is a vector of R2, where R2[i] is the number of successes for the i-th slice
+        THETAS MUST BE WITH VALUES. Where sum(thetas[i..n]) = 1
+        */
+
+       for(int i=0;i<k;i++){
+           for(int j=0;j<n;j++){
+               results[i][belongsTo(thetas)]++;
+           }
+       }
 
     }
     void muestreoExponencial(){
